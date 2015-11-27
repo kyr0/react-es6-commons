99ed5d47ac20cc19b1e4b8a5fbda2df87d3c572b
kyr0 <aron.homberg@gmail.com>
2015-11-27 14:31:16 +0100

    Missing imports

--- gen/index.js
+++ gen/index.js
@@ -16,6 +16,14 @@ var _Component = require('./Component');
 
 var _Component2 = _interopRequireDefault(_Component);
 
+var _Session = require('./Session');
+
+var _Session2 = _interopRequireDefault(_Session);
+
+var _Database = require('./Database');
+
+var _Database2 = _interopRequireDefault(_Database);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = {
@@ -23,5 +31,5 @@ exports.default = {
     AppModule: _AppModule2.default,
     Component: _Component2.default
 };
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7O2tCQUllO0FBQ1gsT0FBRyxlQUFBO0FBQ0gsYUFBUyxxQkFBQTtBQUNULGFBQVMscUJBQUE7Q0FDWiIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBBcHAgZnJvbSAnLi9BcHAnO1xuaW1wb3J0IEFwcE1vZHVsZSBmcm9tICcuL0FwcE1vZHVsZSc7XG5pbXBvcnQgQ29tcG9uZW50IGZyb20gJy4vQ29tcG9uZW50JztcblxuZXhwb3J0IGRlZmF1bHQge1xuICAgIEFwcCxcbiAgICBBcHBNb2R1bGUsXG4gICAgQ29tcG9uZW50XG59Il19
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7a0JBTWU7QUFDWCxPQUFHLGVBQUE7QUFDSCxhQUFTLHFCQUFBO0FBQ1QsYUFBUyxxQkFBQTtDQUNaIiwiZmlsZSI6ImluZGV4LmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IEFwcCBmcm9tICcuL0FwcCc7XG5pbXBvcnQgQXBwTW9kdWxlIGZyb20gJy4vQXBwTW9kdWxlJztcbmltcG9ydCBDb21wb25lbnQgZnJvbSAnLi9Db21wb25lbnQnO1xuaW1wb3J0IFNlc3Npb24gZnJvbSAnLi9TZXNzaW9uJztcbmltcG9ydCBEYXRhYmFzZSBmcm9tICcuL0RhdGFiYXNlJztcblxuZXhwb3J0IGRlZmF1bHQge1xuICAgIEFwcCxcbiAgICBBcHBNb2R1bGUsXG4gICAgQ29tcG9uZW50XG59Il19
 //# sourceMappingURL=index.js.map
